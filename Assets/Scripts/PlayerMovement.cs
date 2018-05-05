f9c05503e9a115047ae86cbc64dec16dcfe5f106
Ben Jammin <benjamin.j.langerak@gmail.com>
2018-05-04 22:38:23 -0400

    removed some campcult for compiler errors, started adding plane math for planes

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -38,7 +38,7 @@ using UnityEngine;
 		//boost
 		//
 		[HideInInspector]
-		public float currentAngle;
+		public float currentDotProduct;
 		[HideInInspector]
 		public float currentVelocity;
 		[HideInInspector]
@@ -103,89 +103,53 @@ using UnityEngine;
 
 
 
-	Vector3 inputAxis;
-	public Camera camera;
-	public float speed = 1.0f;
-	public float angle = 1.0f;
-	private Vector3 lastPosition;
-	private Vector3 positionChange;
-	private Vector3 mousePos;
-	private float velocity;
-
-	public static int numLives;
-
-	public static float distTraveled;
-
-	public float rotationSpeed = 5;
-
-	void Awake()
-	{
-		
-	}
-
-	void Start () 
-	{
-		inputAxis = new Vector3(0,0,0);
-		numLives = 50;
-
-	}
-
-	void Update () 
-	{
-		distTraveled = transform.parent.transform.localPosition.z;
-
-		lastPosition = transform.position;
-
-		Ray ray = camera.ScreenPointToRay(Input.mousePosition);
-		RaycastHit hit;
-
-
-			if (Physics.Raycast(ray, out hit)) 
-			{
-				mousePos = new Vector3(hit.point.x,hit.point.y,transform.position.z);
-				transform.position = Vector3.Lerp(transform.position, mousePos, Time.deltaTime);
-			}
-
-		
-
-
-		positionChange = transform.position - lastPosition;
-		velocity = positionChange.magnitude;
-		angle = velocity * 3;
-		if (positionChange.x <= 0) 
-			angle *= -1;
-		if (mousePos.x - transform.position.x <= 0.3f)
-			transform.RotateAround(transform.forward, GetAngle(transform.position, mousePos)*Time.deltaTime);
-		if (mousePos.x - transform.position.x >= 0.3f)
-			transform.RotateAround(transform.forward, GetAngle(transform.position, mousePos)*Time.deltaTime *-1);
-	}
-
-	float GetAngle(Vector3 from, Vector3 to)
-	{
-		from = new Vector3(from.x, from.y, 20);
-		to = new Vector3(to.x, to.y, 20);
-		return Vector3.Angle(from, to);
-	}
 
 
 
 
+		void Awake()
+		{
+			
+		}
 
-	void SmoothLookAt(Vector3 target, float smooth)
-	{
-		Vector3 dir = target - transform.position + transform.up;
-		Quaternion targetRotation = Quaternion.LookRotation(dir);
-		targetRotation.x = 0;
-		targetRotation.y = 0;
-		transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, Time.deltaTime * smooth);
-	}
+		void Start () 
+		{
 
-	void OnGameOver()
-	{
-		distTraveled = 0;
-		numLives = 50;
-	}
-		
+
+		}
+
+		void Update () 
+		{
+
+		}
+
+		float GetAngle(Vector3 from, Vector3 to)
+		{
+			from = new Vector3(from.x, from.y, 20);
+			to = new Vector3(to.x, to.y, 20);
+			return Vector3.Angle(from, to);
+		}
+
+
+		float GetPlaneDotProduct()
+		{
+			return Vector3.Dot (Vector3.up, transform.forward);
+		}
+
+		void SmoothLookAt(Vector3 target, float smooth)
+		{
+			Vector3 dir = target - transform.position + transform.up;
+			Quaternion targetRotation = Quaternion.LookRotation(dir);
+			targetRotation.x = 0;
+			targetRotation.y = 0;
+			transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, Time.deltaTime * smooth);
+		}
+
+		void OnGameOver()
+		{
+
+		}
+			
 		
 		
 
