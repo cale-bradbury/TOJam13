b0a4863c2743df8a571d37a93553d69e05ac6acf
Ben Jammin <benjamin.j.langerak@gmail.com>
2018-05-05 21:49:15 -0400

    kinda fixed camera, added UI

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -8,16 +8,39 @@ public class CameraFollow : MonoBehaviour
 
 	public float smoothSpeed = 0.125f;
 	public float smoothLookSpeed = 0.125f;
+
+	public float minY, maxY;
 	public Vector3 offset;
+	Vector3 m_targetPosition = Vector3.zero;
 
-	void FixedUpdate ()
+	void LateUpdate ()
 	{
-		Vector3 desiredPosition = target.position + offset;
-		Vector3 smoothedPosition = Vector3.Lerp (transform.position, desiredPosition, smoothSpeed);
-		transform.position = smoothedPosition;
+//		Vector3 desiredPosition = target.position + offset;
+//
+//		Vector3 smoothedPosition = Vector3.Lerp (transform.position, desiredPosition, smoothSpeed * Time.deltaTime);
+//
+//		if (smoothedPosition.y < minY)
+//			smoothedPosition.y = minY;
+//		transform.position = smoothedPosition;
+//
+//		Quaternion targetRot = targetRotation.localRotation;
+//		transform.localRotation = Quaternion.Slerp (transform.localRotation, targetRot, smoothLookSpeed * Time.deltaTime);
+//
 
-		Quaternion targetRot = targetRotation.localRotation;
-		transform.localRotation = Quaternion.Slerp (transform.localRotation, targetRot, smoothLookSpeed);
+		// Get the inverse of the players velocity
+		Vector3 direction = -(target.transform.GetComponent<Rigidbody> ().velocity.normalized);
+
+		//  Set the position of the camera relative to the player, with some distance and height
+		m_targetPosition = target.transform.position + (direction * offset.z) + (Vector3.up * offset.y);
+
+
+
+		// Set camera position                
+		transform.position = m_targetPosition;
+
+		// Let the camera look at the player                    
+//		SmoothLookAt (target.position, smoothLookSpeed);
+		transform.LookAt (target.transform);
 
 	}
 
